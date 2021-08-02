abef659f807a02da01108f8f0366c5c8307cf54f
liudeshun <admin@w3h5.com>
2021-08-02 14:07:07 +0800

    入口文件增加注释

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: Deshun
+ * @Date: 2021-08-02 08:55:37
+ * @LastEditors: Deshun
+ * @LastEditTime: 2021-08-02 14:06:37
+ * @FilePath: \react-demo\src\index.js
+ * @Description: 入口文件
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -5,6 +13,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
+  // 代码检查
   <React.StrictMode>
     <App />
   </React.StrictMode>,
@@ -14,4 +23,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// 记录页面性能（性能监测）
 reportWebVitals();
