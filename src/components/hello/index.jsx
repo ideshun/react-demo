47133d5427340302daa4700adef9adbc5c28abb8
liudeshun <admin@w3h5.com>
2021-08-02 17:05:45 +0800

    删除无用代码及文件，整理目录结构

---
+++ src/components/hello/index.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './index.less';
+
+export default class Hello extends Component {
+  render () {
+    return <h1 className="title">Hello, www.w3h5.com!</h1>
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,26 +2,16 @@
  * @Author: Deshun
  * @Date: 2021-08-02 08:55:37
  * @LastEditors: Deshun
- * @LastEditTime: 2021-08-02 14:06:37
+ * @LastEditTime: 2021-08-02 14:20:34
  * @FilePath: \react-demo\src\index.js
  * @Description: 入口文件
  */
+// 引入 React 核心库
 import React from 'react';
+// 引入 ReactDom
 import ReactDOM from 'react-dom';
-import './index.css';
+// 引入 App 组件
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  // 代码检查
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// 记录页面性能（性能监测）
-reportWebVitals();
+// 渲染 App 到页面
+ReactDOM.render(<App />, document.getElementById('root'));
