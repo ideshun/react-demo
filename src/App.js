03d97d0b2b71f1a8fab192dfbd72801c2758130f
Deshun <job@w3h5.com>
2021-08-03 23:03:42 +0800

    useState & useEffect

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
  * @Author: Deshun
  * @Date: 2021-08-02 08:55:37
  * @LastEditors: Deshun
- * @LastEditTime: 2021-08-02 14:51:28
- * @FilePath: \react-demo\src\App.js
+ * @LastEditTime: 2021-08-03 22:30:13
+ * @FilePath: \React\react-demo\src\App.js
  * @Description: App "外壳"组件
  */
 import React, { Component } from 'react'
 import Hello from './components/hello'
+import { StateHook, EffectHook } from './components/hooks'
 
 // 创建并暴露 App 组件
 export default class App extends Component {
@@ -15,6 +16,8 @@ export default class App extends Component {
     return (
       <div>
         <Hello />
+        <StateHook />
+        <EffectHook />
       </div>
     )
   }
---
+++ src/components/hooks/index.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react'
+
+function StateHook () {
+  const [count, setCount] = useState(0)
+  function add() {
+    // 1.完整写法
+    // setCount((count) => {
+    //   return count + 1
+    // })
+
+    // 2.精简写法
+    // setCount(count => count + 1)
+
+    // 3.简单写法
+    setCount(count + 1)
+  }
+  return (
+    <div>
+      <h1>useState当前值为：{count}</h1>
+      <button onClick={add}>点我 + 1</button>
+    </div>
+  );
+}
+
+// useEffect副作用
+function EffectHook () {
+  const [count, setCount] = useState(0);
+  function unMount() {
+    React.unmountComponentAtNode(document.getElementById('root'))
+  }
+  console.log("@")
+  useEffect(() => {
+    // 在此可以执行任何带副作用操作
+    let timer = setInterval(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+    console.log("@@");
+    return () => {
+      // 组件卸载前调用，在此做一些收尾工作, 比如清除定时器/取消订阅等
+      console.log("##");
+      clearInterval(timer);
+    };
+  }, []); // 不写 [] 全都监控，空数组 谁都不监控，[count] 监控 count 值。
+  // 在两个时刻输出：1.组件挂载，2.监控的值更新
+  return (
+    <div>
+      <h1>useEffect当前值为：{count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        点我 + 1
+      </button>
+      <button onClick={unMount}>卸载组件</button>
+    </div>
+  );
+}
+
+export { StateHook, EffectHook };
