0e9a623d23ef3fbebf46a031254dc709f1540401
liudeshun <admin@w3h5.com>
2021-08-04 11:25:12 +0800

    一个简单的Hello组件

--- src/components/hello/index.jsx
+++ src/components/hello/index.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import './index.less';
+import styles from './index.less';
 
 export default class Hello extends Component {
   render () {
-    return <h1 className="title">Hello, www.w3h5.com!</h1>
+    return <h1 className={styles.title}>Hello, www.w3h5.com!</h1>
   }
 }
