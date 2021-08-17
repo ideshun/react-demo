3a8e6ad9724a27334b5df5c4e84fcb305e7023e6
liudeshun <admin@w3h5.com>
2021-08-17 18:28:50 +0800

    React Hooks Demo

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@
  * @Author: Deshun
  * @Date: 2021-08-02 08:55:37
  * @LastEditors: Deshun
- * @LastEditTime: 2021-08-16 23:23:55
- * @FilePath: \PanSoft\React\react-demo\src\App.js
+ * @LastEditTime: 2021-08-17 16:24:26
+ * @FilePath: \react-demo\src\App.js
  * @Description: App "外壳"组件
  */
 import React, { Fragment } from 'react'
@@ -11,6 +11,9 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import HooksDemo from './pages/hooksDemo'
 import Hello from './pages/hello'
 import Context from './pages/context'
+import UseState from './pages/hooks/useState'
+import UseEffect from './pages/hooks/useEffect'
+import UseLayoutEffect from './pages/hooks/useLayoutEffect'
 
 // 创建并暴露 App 组件
 export default function App() {
@@ -37,6 +40,21 @@ export default function App() {
       </ul>
     </Fragment>
   );
+  const Hooks = () => (
+    <Fragment>
+      <ul>
+        <li>
+          <Link to="/useState">useState</Link>
+        </li>
+        <li>
+          <Link to="/useEffect">useEffect</Link>
+        </li>
+        <li>
+          <Link to="/useLayoutEffect">useLayoutEffect</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
   const hello = () => (
     <Fragment>
       <Hello />
@@ -52,14 +70,33 @@ export default function App() {
       <Context />
     </Fragment>
   );
+  const useState = () => (
+    <Fragment>
+      <UseState />
+    </Fragment>
+  );
+  const useEffect = () => (
+    <Fragment>
+      <UseEffect />
+    </Fragment>
+  );
+  const useLayoutEffect = () => (
+    <Fragment>
+      <UseLayoutEffect />
+    </Fragment>
+  );
   return (
     <Router>
       {/* Link 必须在 Router 标签内 */}
-      <Link to="/">Home</Link>
+      <Link to="/">Home</Link> | <Link to="/hooks">Hooks</Link>
       <Route path="/" exact component={Home} />
       <Route path="/hooksDemo" component={hooksDemo} />
       <Route path="/hello" component={hello} />
       <Route path="/context" component={context} />
+      <Route path="/hooks" component={Hooks} />
+      <Route path="/useState" component={useState} />
+      <Route path="/useEffect" component={useEffect} />
+      <Route path="/useLayoutEffect" component={useLayoutEffect} />
     </Router>
   )
 }
--- src/pages/hooks/useState/index.jsx
+++ src/pages/hooks/useState/index.jsx
@@ -2,7 +2,47 @@
  * @Author: Deshun
  * @Date: 2021-08-16 22:49:00
  * @LastEditors: Deshun
- * @LastEditTime: 2021-08-16 22:49:01
- * @FilePath: \PanSoft\React\react-demo\src\pages\hooks\useState.tsx
- * @Description: 文件描述
+ * @LastEditTime: 2021-08-17 18:23:08
+ * @FilePath: \react-demo\src\pages\hooks\useState\index.jsx
+ * @Description: React State Hook
  */
+
+import React, { Component, useState } from "react";
+
+// 类式组件
+
+// class UseState extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 0,
+//     };
+//   }
+//   add = () => {
+//     console.log(this);
+//     this.setState((state) => ({ count: state.count + 1 }));
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <h2>当前求和为 {this.state.count}</h2>
+//         <button onClick={this.add}>+1</button>
+//       </div>
+//     );
+//   }
+// }
+
+// 函数组件
+const UseState = () => {
+  const [count, setCount] = useState(0);
+  function add() {
+    setCount(value =>  value + 1);
+  };
+  return (
+    <div>
+      <h2>当前求和为 {count}</h2>
+      <button onClick={add}>+1</button>
+    </div>
+  );
+};
+export default UseState;
